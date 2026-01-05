b2607a55c0561d5d8ac1332c4ac889eac3c54a78
lwidm <lukas@widmer.id.au>
2026-01-05 01:41:54 +0100

    deleted old files

--- core.py
+++ core.py
@@ -5,30 +5,52 @@ from dataclasses import dataclass
 
 @dataclass
 class GearData:
+    # module (DE: Modul)
     m: float
+    # number of teeth (DE: Zähnezahl)
     z: int
+    # face width (DE: Zahnbreite) - the axial/z-direction thickness
     b: float
 
+    # pressure angle [degrees] (DE: Eingriffswinkel [grad])
     alpha: float
+    # pressure angle [radian] (DE: Eingriffswinkel [radian])
     alpha_r: float
+    # helix angle at pitch circle [degrees] (DE: Schrägungswinkel am Teilkreis [raidan])
     beta: float
+    # helix angle at pitch circle [radian] (DE: Schrägungswinkel am Teilkreis [radian])
     beta_r: float
+    # helix angle at base circle [degrees] (DE: Schrägungswinkel am Grundkreis [degrees])
     beta_b: float
+    # helix angle at base circle [radian] (DE: Schrägungswinkel am Grundkreis [radian])
     beta_b_r: float
 
+    # addendum coefficient (DE: Kopfhöhenfaktor)
     ha_star: float
+    # clearance coefficient (DE: Kopfspielfaktor)
     c_star: float
+    # fillet radius coefficent (DE: Fussrundingsfaktor)
     rho_f_star: float
+    # profile shift coefficient (DE: Profilverschiebung)
+    x: float
 
+    # addendum (DE: Zahnkopfhöhe)
     ha: float
+    # dedendum (DE: Zahnfusshöhe)
     hf: float
+    # fillet radius at tip (DE: Fussrundung)
     rho_f: float
 
+    # pitch diameter (DE: Teilkreisdurchmesser)
     d: float
+    # base diameter (DE: Grundkreisdurchmesser)
     db: float
+    # tip/addendum diameter (DE: Kopfkreisdurchmesser)
     da: float
+    # root diameter (DE: Fusskreisdurchmesser)
     df: float
 
+    # pitch (DE: Teilung)
     p: float
 
 
@@ -54,6 +76,7 @@ def compute_gear_data(
     ha_star: float,
     c_star: float,
     rho_f_star: float,
+    x: float,
 ) -> GearData:
     alpha_r: float = np.radians(alpha)
     beta_r: float = np.radians(beta)
@@ -63,8 +86,8 @@ def compute_gear_data(
     beta_b_r: float = np.arctan(np.tan(beta_r) * np.cos(alpha_r))
     beta_b: float = np.degrees(beta_b_r)
 
-    ha: float = ha_star * m
-    hf: float = (ha_star + c_star) * m
+    ha: float = (ha_star + x) * m
+    hf: float = (ha_star + c_star - x) * m
     rho_f: float = abs(rho_f_star) * m
 
     d: float = m * float(z)
@@ -85,6 +108,7 @@ def compute_gear_data(
         ha_star=ha_star,
         c_star=c_star,
         rho_f_star=rho_f_star,
+        x=x,
         ha=ha,
         hf=hf,
         rho_f=rho_f,
